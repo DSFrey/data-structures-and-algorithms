eb8aeed71e2cfa54cc5b5e5ad0ac9df73a5e90fb
Daniel Frey <daniel.stephen.frey@gmail.com>
2022-07-29 13:19:10 -0500

    Solve challenge 2

--- javascript/code-challenges/challenges-05.test.js
+++ javascript/code-challenges/challenges-05.test.js
@@ -20,9 +20,7 @@ Write a function named addValues that, given an array of numbers as input, uses 
 
 ------------------------------------------------------------------------------------------------ */
 
-const addValues = (arr) => {
-  // Solution code here...
-};
+const addValues = (arr) => arr.reduce((previous, current) => previous + current, 0);
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 3
