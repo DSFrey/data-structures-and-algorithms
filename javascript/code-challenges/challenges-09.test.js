8d78cb2c3d25d6a0c192f117700446c5b6c94ae1
Daniel Frey <daniel.stephen.frey@gmail.com>
2022-08-04 15:09:36 -0500

    Solve challenge 7

--- javascript/code-challenges/challenges-09.test.js
+++ javascript/code-challenges/challenges-09.test.js
@@ -141,7 +141,11 @@ The input and output of this function are the same as the input and output from 
 ------------------------------------------------------------------------------------------------ */
 
 const hasChildrenEntries = (arr, character) => {
-  // Solution code here...
+  let hasChildren = false;
+  arr.forEach(char => {
+    if (Object.entries(char)[0][1] === character && Object.entries(char)[2][0] === 'children') {hasChildren = true;}
+  });
+  return hasChildren;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -260,7 +264,7 @@ describe('Testing challenge 6', () => {
   });
 });
 
-xdescribe('Testing challenge 7', () => {
+describe('Testing challenge 7', () => {
   test('It should return true for characters that have children', () => {
     expect(hasChildrenEntries(characters, 'Eddard')).toBeTruthy();
   });
