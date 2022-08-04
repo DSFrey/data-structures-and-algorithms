3a5809fff06d850eebae5ad39a6dc391b917054b
Daniel Frey <daniel.stephen.frey@gmail.com>
2022-08-04 13:52:09 -0500

    Solve challenge 4

--- javascript/code-challenges/challenges-09.test.js
+++ javascript/code-challenges/challenges-09.test.js
@@ -53,9 +53,7 @@ HR has asked you to change the data to make it easier to print so that it looks 
 
 ------------------------------------------------------------------------------------------------ */
 
-const updateNumbers = (obj) => {
-  // Solution code here...
-};
+const updateNumbers = (obj) => Object.entries(obj).map(arr => `${arr[0]}: ${arr[1]}`);
 
 
 
