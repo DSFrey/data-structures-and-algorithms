faf5bfec79d14fc04fc5821569e7ead21ec96297
Daniel Frey <daniel.stephen.frey@gmail.com>
2022-08-05 13:36:43 -0500

    Solve challenge 6

--- javascript/code-challenges/challenges-10.test.js
+++ javascript/code-challenges/challenges-10.test.js
@@ -118,8 +118,16 @@ const errands = [
 ];
 
 const howManyTreats = (arr) => {
-  // Solution code here...
-};
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].store === 'Pet store') {
+      for (let j = 0; j < arr[i].items.length; j++) {
+        if (arr[i].items[j].name === 'Treats') {
+          return arr[i].items[j].quantity;
+        }
+      }
+    }
+  }
+}
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 7 - Stretch Goal
