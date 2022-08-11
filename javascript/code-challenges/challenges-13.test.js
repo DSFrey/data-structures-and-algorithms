47d2e3c7c876468489e305e513172dc8caf8854b
Daniel Frey <daniel.stephen.frey@gmail.com>
2022-08-10 22:09:07 -0500

    Solve problem 2

--- javascript/code-challenges/challenges-13.test.js
+++ javascript/code-challenges/challenges-13.test.js
@@ -19,9 +19,7 @@ Write a function named firstLetters that takes in an array of strings and return
 For example, ['this is great :)', 'wow', 'whyyyyyy :(', ':)))))'] returns ['t', 'w', 'w', ':']
 ------------------------------------------------------------------------------------------------ */
 
-const firstLetters = (arr) => {
-  // Solution code here...
-};
+const firstLetters = (arr) => arr.map(string => string.charAt(0));
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 3
