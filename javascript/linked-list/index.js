6533c2c0383a30e8ed5ae30dad857decbc1655f4
Daniel Frey <daniel.stephen.frey@gmail.com>
2022-09-26 03:32:14 +0000

    Implement linked list

--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,7 +1,74 @@
 'use strict';
 
-class LinkedList {
 
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
-module.exports = LinkedList;
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  /**
+   * Adds new node to the end of the list
+   * @param {*} value
+   * @returns
+   */
+  add(value) {
+    const node = new Node(value);
+    if (!this.head) {
+      this.head = node;
+      return;
+    }
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+    current.next = node;
+  }
+
+  /**
+   * Adds new node to the beginning of the list
+   * @param {*} value
+   */
+  insert(value) {
+    const node = new Node(value);
+    node.next = this.head;
+    this.head = node;
+  }
+
+  /**
+   * Checks if list contains the search value
+   * @param {*} value
+   * @returns
+   */
+  includes(value) {
+    let current = this.head;
+    while (current) {
+      if (current.value === value) return true;
+      current = current.next;
+    }
+    return false;
+  }
+
+  /**
+   *
+   * @returns Returns string representing all values in the list
+   */
+  toString() {
+    let current = this.head;
+    let result = current.value;
+    while (current.next) {
+      current = current.next;
+      result += ' -> ' + current.value;
+    }
+    result += ' -> NULL';
+    return result;
+  }
+}
+
+module.exports = { LinkedList, Node };
