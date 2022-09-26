7fa53390b134b8d93880e671da97daaa8b575c3e
Daniel Frey <daniel.stephen.frey@gmail.com>
2022-09-26 03:32:37 +0000

    Add linked list tests

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -1,10 +1,46 @@
 'use strict';
 
 // Require our linked list implementation
-const LinkedLilst = require('../index');
+const {LinkedList} = require('../index');
 
 describe('Linked List', () => {
-  it('works', () => {
-    expect(true).toBeTruthy();
+  test('Instantiate an empty linked list', () => {
+    let newList = new LinkedList();
+    expect(newList).toBeTruthy();
+  });
+  test('Insert into the linked list', ()=>{
+    let newList = new LinkedList();
+    newList.insert(1);
+    newList.insert(2);
+    expect(newList.head.value).toEqual(2);
+  });
+  // test('Head property points to the first node in the linked list', ()=>{
+  //   I don't know how to test this; the head is always the first item in the list, that's how the list is defined.
+  // })
+  test('Insert multiple nodes into the linked list', ()=>{
+    let newList = new LinkedList();
+    newList.insert(1);
+    newList.insert(2);
+    expect(newList.head.value).toEqual(2);
+    expect(newList.head.next.value).toEqual(1);
+  });
+  test('Return true when finding a value that is in the linked list',()=>{
+    let newList = new LinkedList();
+    newList.insert(1);
+    newList.insert(2);
+    expect(newList.includes(2)).toBeTruthy();
+  });
+  test('Return false when finding a value that is not in the linked list',()=>{
+    let newList = new LinkedList();
+    newList.insert(1);
+    newList.insert(2);
+    expect(newList.includes(3)).toBeFalsy();
+  });
+  test('Return a collection of all the values that exist in the linked list',()=>{
+    let newList = new LinkedList();
+    newList.insert('{ c }');
+    newList.insert('{ b }');
+    newList.insert('{ a }');
+    expect(newList.toString()).toEqual('{ a } -> { b } -> { c } -> NULL');
   });
 });
