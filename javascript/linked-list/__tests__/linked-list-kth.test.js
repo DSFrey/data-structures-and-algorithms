f93638b98c37b3fb597470c708f52e164f0cdada
Daniel Frey <daniel.stephen.frey@gmail.com>
2022-09-28 00:21:33 +0000

    Write test for middleNode method

--- javascript/linked-list/__tests__/linked-list-kth.test.js
+++ javascript/linked-list/__tests__/linked-list-kth.test.js
@@ -13,18 +13,21 @@ describe('Linked List method kthFromEnd', () => {
   test('Find the node k positions from the end', () => {
     expect(newList.kthFromEnd(2)).toEqual(2);
   });
-  test('Give exception when k is greater than the length of list',()=>{
+  test('Give exception when k is greater than the length of list', () => {
     expect(newList.kthFromEnd(6)).toEqual('Exception');
   });
-  test('Give exception when length of list and k are the same',()=>{
+  test('Give exception when length of list and k are the same', () => {
     expect(newList.kthFromEnd(5)).toEqual('Exception');
   });
-  test('Give exception when k is negative',()=>{
+  test('Give exception when k is negative', () => {
     expect(newList.kthFromEnd(-5)).toEqual('Exception');
   });
-  test('Functions when length of list is 1',()=>{
+  test('Functions when length of list is 1', () => {
     let oneNode = new LinkedList();
     oneNode.insert(1);
     expect(oneNode.kthFromEnd(5)).toEqual('Exception');
+  });
+  test('Find middle node', () => {
+    expect(newList.middleNode()).toEqual(2);
   });
 });
