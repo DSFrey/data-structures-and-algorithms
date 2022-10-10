2a050c595ee4348e4a3d382ff23b4d7d45fcf0b8
Daniel Frey <daniel.stephen.frey@gmail.com>
2022-10-10 16:29:45 +0000

    Add SearchTree class

--- javascript/trees/index.js
+++ javascript/trees/index.js
@@ -38,5 +38,55 @@ class BinaryTree {
 }
 
 class SearchTree extends BinaryTree {
+  add(newValue) {
+    let currentNode = this.root;
+    let search = (value) => {
+      if (value === currentNode.value) throw new Error('Value already exists');
+      if (value < currentNode.value) {
+        if (currentNode.left) {
+          currentNode = currentNode.left;
+          search(value);
+        } else {
+          currentNode.left = new Node(value);
+          return;
+        }
+      }
+      if (value > currentNode.value) {
+        if (currentNode.right) {
+          currentNode = currentNode.right;
+          search(value);
+        } else {
+          currentNode.right = new Node(value);
+          return;
+        }
+      }
+    };
+    search(newValue);
+  }
 
+  contains(searchValue) {
+    let currentNode = this.root;
+    let search = (value) => {
+      if (value === currentNode.value) return true;
+      if (value < currentNode.value) {
+        if (currentNode.left) {
+          currentNode = currentNode.left;
+          search(value);
+        } else {
+          return false;
+        }
+      }
+      if (value > currentNode.value) {
+        if (currentNode.right) {
+          currentNode = currentNode.right;
+          search(value);
+        } else {
+          return false;
+        }
+      }
+    };
+    return search(searchValue);
+  }
 }
+
+module.exports = { BinaryTree, SearchTree };
