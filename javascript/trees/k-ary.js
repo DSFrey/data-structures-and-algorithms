5536139722eb3f16502b12ee53b999b21d6677eb
Daniel Frey <daniel.stephen.frey@gmail.com>
2022-10-12 21:56:22 +0000

    Create fizzBuzzTree function

--- javascript/trees/k-ary.js
+++ javascript/trees/k-ary.js
@@ -16,3 +16,27 @@ class KaryTree {
   }
 }
 
+function fizzBuzzTree(originalTree) {
+  if (originalTree.root === null) return new KaryTree();
+
+  let newTree = new KaryTree();
+  let fizzBuzz = (value) => {
+    if (!(value % 3) && !(value % 5)) return 'FizzBuzz';
+    if (!(value % 3)) return 'Fizz';
+    if (!(value % 5)) return 'Buzz';
+    return `${value}`;
+  };
+
+  newTree.root = new KaryNode(fizzBuzz(originalTree.root.value));
+
+  let traverse = (oldNode, newNode) => {
+    for (let i = 0; i < oldNode.children.length; i++) {
+      newNode.addChild(fizzBuzz(oldNode.children[i].value));
+      traverse(oldNode.children[i], newNode.children[i]);
+    }
+  };
+  traverse(originalTree.root, newTree.root);
+  return newTree;
+}
+
+module.exports = { KaryNode, KaryTree, fizzBuzzTree };
