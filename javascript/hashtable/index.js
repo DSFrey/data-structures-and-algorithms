cdaa4584ff2ba8714e91cff67cbfc2950098fb47
Daniel Frey <daniel.stephen.frey@gmail.com>
2022-10-31 03:30:47 +0000

    Create hashtable class with hash, get, and set methods

---
+++ javascript/hashtable/index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+class Hashtable {
+  constructor(size) {
+    this.size = size;
+    this.buckets = new Array(size);
+  }
+
+  hash(key) {
+    let characters = key.split('');
+    let charSum = characters.reduce((sum, char) => sum + char.charCodeAt(0), 0);
+    return (charSum * 911) % this.size;
+  }
+
+  get(key) {
+    let index = this.hash(key);
+    for (let i = 0; i < this.buckets[index].length; i++) {
+      if (this.buckets[index][i][0] === key) return this.buckets[index][i][1];
+    }
+    return null;
+  }
+
+  set(key, value) {
+    let index = this.hash(key);
+    if (!this.buckets[index]) this.buckets[index] = [];
+    for (let i = 0; i < this.buckets[index].length; i++) {
+      if (this.buckets[index][i][0] === key) {
+        this.buckets[index][i][1] = value;
+        return;
+      }
+    }
+    this.buckets[index].push([key, value]);
+  }
+}
+
+module.exports = Hashtable;
