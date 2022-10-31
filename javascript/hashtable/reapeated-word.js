6963a15633b41b6c622e8de4e1f4e51a21efeae5
Daniel Frey <daniel.stephen.frey@gmail.com>
2022-10-31 23:05:32 +0000

    Add countByWord function

--- javascript/hashtable/reapeated-word.js
+++ javascript/hashtable/reapeated-word.js
@@ -13,3 +13,18 @@ function firstRepeat(inputString) {
   return null;
 }
 
+function countByWord(inputString) {
+  let wordArray = inputString.match(/\b\w+\b/);
+  let wordHash = new Hashtable(32);
+  wordArray.forEach(word => {
+    if (wordHash.has(word)) {
+      let total = wordHash.get(word);
+      wordHash.set(word, total + 1);
+    } else {
+      wordHash.set(word, 1);
+    }
+  });
+  const wordList = wordHash.keys();
+  const wordCount = wordList.map(word => [word, wordHash.get(word)]);
+  return wordCount;
+}
