73afc0a1cd0ac6c2feb3dfd43c61d610c0f23d0b
Daniel Frey <daniel.stephen.frey@gmail.com>
2022-11-01 22:04:13 +0000

    Add condition to treeIntersection for empty trees

--- javascript/hashtable/tree-intersection.js
+++ javascript/hashtable/tree-intersection.js
@@ -12,23 +12,16 @@ function inOrderCallback(tree, callback) {
 }
 
 function treeIntersection(tree1, tree2) {
+  if (!tree1.root || !tree2.root) return [];
   let table = new Hashtable(32);
   let intersection = [];
-  // let traverse1 = (node) => {
-  //   table.set(node.value);
-  //   if (node.left) traverse1(node.left);
-  //   if (node.right) traverse1(node.right);
-  // };
-  // let traverse2 = (node) => {
-  //   if (table.has(node.value)) intersection.push(node.value);
-  //   if (node.left) traverse2(node.left);
-  //   if (node.right) traverse2(node.right);
-  // };
-  inOrderCallback(tree1, (value) => table.set(value));
+  inOrderCallback(tree1, (value) => {
+    table.set(`${value}`);
+  });
   inOrderCallback(tree2, (value) => {
-    if (table.has(value)) intersection.push(value);
+    if (table.has(`${value}`)) intersection.push(value);
   });
   return intersection;
 }
 
-module.exports = treeIntersection;
+module.exports = { inOrderCallback, treeIntersection };
