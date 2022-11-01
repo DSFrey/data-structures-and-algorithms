dae9d364756ffbb6b8b78b9a9ebb0efb0c72f718
Daniel Frey <daniel.stephen.frey@gmail.com>
2022-11-01 21:44:13 +0000

    Modularize with inOrderCallback function

--- javascript/hashtable/tree-intersection.js
+++ javascript/hashtable/tree-intersection.js
@@ -2,21 +2,32 @@
 
 const Hashtable = require('.');
 
+function inOrderCallback(tree, callback) {
+  let traverse = (node) => {
+    if (node.left) traverse(node.left);
+    callback(node.value);
+    if (node.right) traverse(node.right);
+  };
+  traverse(tree.root);
+}
+
 function treeIntersection(tree1, tree2) {
   let table = new Hashtable(32);
   let intersection = [];
-  let traverse1 = (node) => {
-    table.set(node.value, 'banana');
-    if (node.left) traverse1(node.left);
-    if (node.right) traverse1(node.right);
-  };
-  let traverse2 = (node) => {
-    if (table.has(node.value)) intersection.push(node.value);
-    if (node.left) traverse2(node.left);
-    if (node.right) traverse2(node.right);
-  };
-  traverse1(tree1.root);
-  traverse2(tree2.root);
+  // let traverse1 = (node) => {
+  //   table.set(node.value);
+  //   if (node.left) traverse1(node.left);
+  //   if (node.right) traverse1(node.right);
+  // };
+  // let traverse2 = (node) => {
+  //   if (table.has(node.value)) intersection.push(node.value);
+  //   if (node.left) traverse2(node.left);
+  //   if (node.right) traverse2(node.right);
+  // };
+  inOrderCallback(tree1, (value) => table.set(value));
+  inOrderCallback(tree2, (value) => {
+    if (table.has(value)) intersection.push(value);
+  });
   return intersection;
 }
 
